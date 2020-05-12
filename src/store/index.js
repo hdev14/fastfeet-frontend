02dee5ad40ef7bb4382f65cdbb15f8406ae80691
HermersonDev <ne.hermerson@gmail.com>
2020-05-12 17:01:56 -0300

    :wrench: add redux-saga into store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
+import rootSaga from './modules/rootSaga';
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
