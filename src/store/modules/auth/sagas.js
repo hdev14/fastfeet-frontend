d6ebab96280bfe7381e0d854cd110b5d5ff8ce2c
HermersonDev <ne.hermerson@gmail.com>
2020-05-13 16:30:06 -0300

    :construction: craete action saga signIn

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import {
+  all, takeLatest, call, put,
+} from 'redux-saga/effects';
 
-export default all([]);
+import api from '../../../services/api';
+import history from '../../../services/history';
+
+import { authTypes } from '../actionsTypes';
+
+import { signInSuccess } from './actions';
+
+function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, '/sessions', { email, password });
+
+    const { token } = response.data;
+
+    yield put(signInSuccess(token));
+    history.push('/order');
+
+  } catch (error) {
+    console.tron.log('ERROR -> ', error);
+    return;
+  }
+}
+
+export default all([
+  takeLatest(authTypes.SIGN_IN_REQUEST, signIn),
+]);
