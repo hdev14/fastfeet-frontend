3dddf746cd9dc61c9821e6245ccae691bb91573c
HermersonDev <ne.hermerson@gmail.com>
2020-05-14 16:26:46 -0300

    :construction: add order from the web server

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoPlus, GoSearch } from 'react-icons/go';
 import { FaCircle } from 'react-icons/fa';
 import { MdMoreHoriz, MdRemoveRedEye, MdCreate } from 'react-icons/md';
 import { IoMdTrash } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import OrderModal from '../../components/OrderModal';
 
@@ -20,6 +22,18 @@ import { Status, DeliverymanInfo } from './styles';
 import handleAction from '../../functions/handleAction';
 
 export default function Order() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      const response = await api.get('/orders');
+
+      setOrders([...response.data]);
+    }
+
+    fetchOrders();
+  }, []);
+
   function handleModal(modalId) {
     const modal = document.getElementById(modalId).firstChild;
     if (modal) {
@@ -66,62 +80,64 @@ export default function Order() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>#01</td>
-            <td>Ludwig van Beethoven</td>
-            <td>
-              <DeliverymanInfo>
-                <Picture src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="" />
-                <span>Jonh Doe</span>
-              </DeliverymanInfo>
-            </td>
-            <td>Rio do Sul</td>
-            <td>Santa Catarina</td>
-            <td>
-              <Status className="green">
-                <FaCircle />
-                entregue
-              </Status>
-            </td>
-            <td>
-              <div id="modal">
-                <OrderModal />
-              </div>
-              <Actions>
-                <MdMoreHoriz size={24} color="#666" onClick={handleAction} />
-                <ul style={{ display: 'none' }}>
-                  <li>
-                    <div
-                      role="button"
-                      onClick={() => handleModal('modal')}
-                      onKeyPress={() => handleModal('modal')}
-                      tabIndex={0}
-                    >
-                      <MdRemoveRedEye size={16} color="#8E5BE8" />
-                      Visualizar
-                    </div>
-                  </li>
-                  <li>
-                    <Link to="/order/edit/1">
-                      <MdCreate size={16} color="#4D85EE" />
-                      Editar
-                    </Link>
-                  </li>
-                  <li>
-                    <div
-                      role="button"
-                      onClick={handleDelete}
-                      onKeyPress={handleDelete}
-                      tabIndex={0}
-                    >
-                      <IoMdTrash size={16} color="#DE3B3B" />
-                      Excluir
-                    </div>
-                  </li>
-                </ul>
-              </Actions>
-            </td>
-          </tr>
+          {orders.map((order, index)=> (
+            <tr key={order.id}>
+              <td>{`#${index}`}</td>
+              <td>{order.product}</td>
+              <td>
+                <DeliverymanInfo>
+                  <Picture src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="" />
+                  <span>{order.recipient.name}</span>
+                </DeliverymanInfo>
+              </td>
+              <td>{order.recipient.city}</td>
+              <td>{order.recipient.state}</td>
+              <td>
+                <Status className="green">
+                  <FaCircle />
+                  entregue
+                </Status>
+              </td>
+              <td>
+                <div id="modal">
+                  <OrderModal />
+                </div>
+                <Actions>
+                  <MdMoreHoriz size={24} color="#666" onClick={handleAction} />
+                  <ul style={{ display: 'none' }}>
+                    <li>
+                      <div
+                        role="button"
+                        onClick={() => handleModal('modal')}
+                        onKeyPress={() => handleModal('modal')}
+                        tabIndex={0}
+                      >
+                        <MdRemoveRedEye size={16} color="#8E5BE8" />
+                        Visualizar
+                      </div>
+                    </li>
+                    <li>
+                      <Link to="/order/edit/1">
+                        <MdCreate size={16} color="#4D85EE" />
+                        Editar
+                      </Link>
+                    </li>
+                    <li>
+                      <div
+                        role="button"
+                        onClick={handleDelete}
+                        onKeyPress={handleDelete}
+                        tabIndex={0}
+                      >
+                        <IoMdTrash size={16} color="#DE3B3B" />
+                        Excluir
+                      </div>
+                    </li>
+                  </ul>
+                </Actions>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
